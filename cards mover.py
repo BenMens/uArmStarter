7c4b1dd798074ee9bc7049a67fec224355c838fe
Ben Mens <ben.mens@gmail.com>
2019-11-16 14:55:01 +0100

    fixes

--- cards mover.py
+++ cards mover.py
@@ -9,7 +9,8 @@ import time
 
 
 #Configure Serial Port
-serialport = "/dev/ttyACM0"  # for linux like system
+# serialport = "/dev/ttyACM0"  # for linux like system
+serialport = "/dev/tty.usbmodem14101"
 
 # Connect to uArm 
 myRobot = uArmRobot.robot(serialport, 1)   # user 0 for firmware < v4 and use 1 for firmware v4
@@ -17,9 +18,14 @@ myRobot.debug = True  # Enable / Disable debug output on screen, by default disa
 myRobot.connect()
 myRobot.mode(0)   # Set mode to Normal
 
-wait = 0
-speed = 100
-numCarts = 7
+wait = 1
+speed = 200
+numCarts = 8
+cardThickness = 4
+
+pos1 = [200, 0, 5]
+
+pos2 = [200, 180, 5]
 
 time.sleep(1)
 
@@ -27,36 +33,36 @@ myRobot.goto(  200, 0, 100, 10)
 
 time.sleep(5)
 
+
 for i in range(0, numCarts):
 
     time.sleep(wait)
 
-    myRobot.goto(  200, 0, 15 - i, speed)
+    myRobot.goto(  pos1[0], pos1[1], pos1[2] + 100, speed)
 
     time.sleep(wait)
 
-    myRobot.goto(  200, 0, (numCarts - i) * 0.5 + 1, speed)
-
-    time.sleep(wait)
+    myRobot.goto(  pos1[0], pos1[1], pos1[2] + (numCarts - i) * cardThickness, speed)
 
     myRobot.pump(True)
 
+    time.sleep(2)
+
+    myRobot.goto(  pos1[0], pos1[1], pos1[2] + 100, speed)
+
     time.sleep(1)
 
-    myRobot.goto(  200, 0, 100, speed)
+    myRobot.goto(  pos2[0], pos2[1], pos2[2] + 100, speed)
 
-    time.sleep(wait)
+    time.sleep(1)
 
-
-    myRobot.goto(  200, 180, 100, speed)
-
-    time.sleep(wait)
-
-    myRobot.goto(  200, 180, 6 + i, speed)
-
-    time.sleep(wait)
+    myRobot.goto(  pos2[0], pos2[1], pos2[2] + (i + 1) * cardThickness + 2, speed)
 
     myRobot.pump(False)
+
+    time.sleep(1)
+
+    myRobot.goto(  pos2[0], pos2[1], pos2[2] + 100, speed)
 
     time.sleep(1)
 
